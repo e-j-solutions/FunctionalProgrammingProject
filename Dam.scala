3fb3c4f5831454d7bd3f433c07fd9556d60f6c11
e-j-solutions <evelina.jermakova@student.lut.fi>
2026-05-03 21:24:13 +0300

    Update Dam.scala

--- Dam.scala
+++ Dam.scala
@@ -1,15 +1,13 @@
+/*Object Dam has 5 floodgates stored in a vector. For the energy output values Dam reads text file (File specified in EnergyData)*/
 import scala.io.Source
 import scala.util.{Try, Using}
 
 object Dam {
 
-  // Always 5 floodgates
   private var floodgates: Vector[Boolean] = Vector.fill(5)(false)
 
-  // Get current state
   def getFloodgates: Vector[Boolean] = floodgates
 
-  // Set a specific gate
   def setFloodgate(index: Int, open: Boolean): Either[String, Unit] =
     if (index < 0 || index >= floodgates.length)
       Left(s"Invalid index: $index")
